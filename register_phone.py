50496cc158c3c797a2d6d2727e4843e938a39bb4
Aleksandr Stevens <arstevens310@gmail.com>
2016-08-03 14:02:13 -0700

    added delete arg in register script

--- register_phone.py
+++ register_phone.py
@@ -6,6 +6,7 @@ def parse_command_line():
 	parser = argparse.ArgumentParser(description="Register a phone for Bluetooth Login")
 	parser.add_argument("phone_id",type=str,help="the identification for your phone (mac address or phone name)") 
 	parser.add_argument("username",type=str,help="The username mapped to your phone name")
+	parser.add_argument("delete",nargs='?',default="n",type=str,help="delete the account specified, put d to delete")
 	args = parser.parse_args()
 	return args
 
@@ -31,16 +32,41 @@ def is_phone_registered(ermrest,phone):
 		return True
 	return False
 
+def delete(ermrest,username):
+	success = bool()
+	query = "/username="+username
+	
+	try:
+		ermrest.delete_data(8,"users",query)
+		success = True
+	except:
+		success = False
+
+	return success
+		
+
 def register():
 	args = parse_command_line()
 	ermrest = ErmrestHandler("ec2-54-172-182-170.compute-1.amazonaws.com","root","root")
+
+	if (args.delete == 'd'): 
+		success = delete(ermrest,args.username)
+		if (success):
+			print("User '{}' successfully deleted".format(args.username))
+			sys.exit(0)
+		else:
+			print("Invalid phone id. User '{}' not deleted".format(args.username))
+			sys.exit(0)
+
 	if (is_username_taken(ermrest,args.username)):
 		print("Username '{}' is taken. Please choose another one".format(args.username))
 		sys.exit(0)
+
 	elif (is_phone_registered(ermrest,args.phone_id)):
 		taken_username = str(ermrest.get_data(8,"users","/phone_identification="+args.phone_id)[0]['username'])
 		print("Phone '{}' is already registered under the username '{}'".format(args.phone_id,taken_username))
 		sys.exit(0)
+
 	new_user_data = {"username":args.username,"phone_identification":args.phone_id}
 	try:
 		ermrest.put_data(8,"users",new_user_data)
